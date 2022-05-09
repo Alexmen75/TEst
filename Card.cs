997d4f88fdb537da9a01db30a241423cb26ebb33
Алексей Белов <alexei_bam@mail.ru>
2022-05-09 16:56:30 +0600

    start

--- Card.cs
+++ Card.cs
@@ -6,17 +6,19 @@ namespace TEst
   [Serializable()]
   public class Card
   {
-    public Card(string name, string path,int health, int damage)
+    public Card(string name, string path,int health, int damage, int energy)
     {
       Name = name;
       Health = health;
       Damage = damage;
       Path = path;
+      Energy = energy;
     }
     public string Name { get; set; }
     public string Path { get; set; }
     public int Health { get; set; }
     public int Damage { get; set; }
+    public int Energy { get; set; }
     
     // public Action SomeAction { get; set; }
   }
--- Program.cs
+++ Program.cs
@@ -11,11 +11,11 @@ namespace TEst
     {
         var cards = new List<Card>()
         {
-          new Card("Handy", "handy",10, 20),
-          new Card("Jock", "jock",20, 10),
-          new Card("RB-42", "rb-42",20, 10),
-          new Card("JP98", "jp98",20, 10),
-          new Card("Lilya", "lilya",20, 10)
+          new Card("Handy", "handy",   10, 20),
+          new Card("Jock",  "jock",    20, 10),
+          new Card("RB-42", "rb-42",   20, 10),
+          new Card("JP98",  "jp98",    20, 10),
+          new Card("Lilya", "lilya" ,  20, 10)
         };
         WriteCard(cards);
         var result = ReadCard();
